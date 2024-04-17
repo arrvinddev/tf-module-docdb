960b695b28e3fe4b00120e28e529c9df86b92e31
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-17 23:17:45 +0530

    terraform module docdb

---
+++ vars.tf
@@ -0,0 +1,18 @@
+
+variable "tags" {}
+variable "env" {}
+variable "subnets"{}
+variable "name"{
+    default = "docdb"
+}
+
+variable "vpc_id"{}
+variable "allow_db_cidr" {}
+variable "kms_arn" {}
+variable "port_no" {
+    default = 27017
+}
+
+
+
+
